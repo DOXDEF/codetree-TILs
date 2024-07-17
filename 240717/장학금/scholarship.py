6128d58c19287e2f4cf7570fa9bdd24fd378e8e0
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-17 13:26:17 +0900

    Title: 장학금, Time: 227ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/장학금/scholarship.py
+++ 240717/장학금/scholarship.py
@@ -1,10 +1,9 @@
 a, b = map(int, input().split())
 if a < 90:
     print(0)
-else:
-    if b >= 95:
+elif b >= 95:
         print(100000)
-    elif b >= 90:
-        print(50000)
-    else:
-        print(0)
+elif b >= 90:
+    print(50000)
+else:
+    print(0)

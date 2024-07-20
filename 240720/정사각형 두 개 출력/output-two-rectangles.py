9706b4a4776233c8221bd4b478dadb3468a47197
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-20 10:41:15 +0900

    Title: 정사각형 두 개 출력, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/정사각형 두 개 출력/output-two-rectangles.py
+++ 240720/정사각형 두 개 출력/output-two-rectangles.py
@@ -1,10 +1,7 @@
 n = int(input())
-for i in range(n):
+for i in range(2):
     for j in range(n):
-        print("*", end = "")
-    print()
-print()
-for i in range(n):
-    for j in range(n):
-        print("*", end = "")
+        for k in range(n):
+            print("*", end = "")
+        print()
     print()

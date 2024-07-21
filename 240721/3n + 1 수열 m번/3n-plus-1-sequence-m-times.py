131fb8335cfc9f121bfdb58c28b9249129731cf6
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-21 15:20:02 +0900

    Title: 3n + 1 수열 m번, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240721/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240721/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -3,12 +3,12 @@ for _ in range(m):
     n = int(input())
     cnt = 0
     while True:
+        if n == 1:
+            print(cnt)
+            break
         if n % 2 == 0:
             n //= 2
             cnt += 1
         else:
             n = n * 3 + 1
             cnt += 1
-        if n == 1:
-            print(cnt)
-            break

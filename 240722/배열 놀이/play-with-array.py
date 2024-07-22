c95635c267e6eff00390e92e5ed0bdb650a9677d
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-22 13:38:50 +0900

    Title: 배열 놀이, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/배열 놀이/play-with-array.py
+++ 240722/배열 놀이/play-with-array.py
@@ -1,3 +1,15 @@
 n, q = map(int, input().split())
 arr = list(map(int, input().split()))
-for _ in range(n):
+for _ in range(q):
+    count_arr = list(map(int, input().split()))
+    if count_arr[0] == 1:
+        print(arr[count_arr[1] - 1])
+    elif count_arr[0] == 2:
+        for i in range(len(arr)):
+            if arr[i] == count_arr[1]:
+                print(i + 1)
+                break
+    else:
+        for i in range(count_arr[1] - 1, count_arr[2]):
+            print(arr[i], end = " ")
+        print()

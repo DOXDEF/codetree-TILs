47e72d4a74adcf3d7656ec3ffd585864bacb24bb
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-22 13:17:21 +0900

    Title: 나눗셈의 나머지, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/나눗셈의 나머지/remainder-of-division.py
+++ 240722/나눗셈의 나머지/remainder-of-division.py
@@ -1,4 +1,11 @@
 a, b = map(int, input().split())
 count_arr = [0 for _ in range(b)]
+sum_cnt = 0
 while True:
     count_arr[a % b] += 1
+    if a // b <= 1:
+        break
+    a //= b
+for i in count_arr:
+    sum_cnt += i * i
+print(sum_cnt)

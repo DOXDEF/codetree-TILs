77b6e153ae41c38ed6887da8f60c27b0eadcb4a2
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-22 10:38:03 +0900

    Title: 학점 계산기, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/학점 계산기/credit-calculator.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(float, input().split()))
+eva = sum(arr) / n
+print(f"{eva:.1f}")
+if eva >= 4.0:
+    print("Perfect")
+elif eva >= 3.0:
+    print("Good")
+else:
+    print("Pood")

ce65f6c3a7181665665bfa493b4a1cf6df324dfc
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-23 13:52:47 +0900

    Title: 동전이 있는 위치, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240723/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+for i in range(m):
+    a, b = map(int, input().split())
+    arr[a - 1][b - 1] = 1
+for i in arr:
+    for j in i:
+        print(j, end = " ")
+    print()

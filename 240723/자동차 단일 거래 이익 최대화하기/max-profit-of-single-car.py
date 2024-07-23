c71939b47ecfbfa222d21c51e49a5980050428c5
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-23 10:44:27 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240723/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240723/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,11 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 answer = 0
-arr = arr[arr.index(min(arr)):]
-if len(arr) == 1:
-    print(0)
-else:
-    for i in arr:
-        if i - min(arr) >= answer:
-            answer = i - min(arr)
-    print(answer)
+while True:
+    if len(arr[arr.index(min(arr)):]) == 1:
+        arr = arr[:arr.index(min(arr))]
+    else:
+        arr = arr[arr.index(min(arr)):]
+        for i in arr:
+            if i - min(arr) >= answer:
+                answer = i - min(arr)
+        print(answer)
+        break

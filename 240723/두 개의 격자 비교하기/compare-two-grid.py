c84ad02bc3f6738119e1f282220b4c5d7bbb97ed
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-23 12:17:21 +0900

    Title: 두 개의 격자 비교하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/두 개의 격자 비교하기/compare-two-grid.py
+++ 240723/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,18 +1,18 @@
 n, m = map(int, input().split())
 answer = [
-    [0 for _ in range(n)]
-    for _ in range(m) 
+    [0 for _ in range(m)]
+    for _ in range(n) 
 ]
 arr_1 = [
     list(map(int, input().split()))
-    for _ in range(m)
+    for _ in range(n)
 ]
 arr_2 = [
     list(map(int, input().split()))
-    for _ in range(m)
+    for _ in range(n)
 ]
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         if arr_1[i][j] != arr_2[i][j]:
             answer[i][j] = 1
 for i in answer:

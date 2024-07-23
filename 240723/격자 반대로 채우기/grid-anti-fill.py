b095755dfaff4ff301e7e15f8ed61e4dfa9e0063
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-23 13:15:56 +0900

    Title: 격자 반대로 채우기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/격자 반대로 채우기/grid-anti-fill.py
+++ 240723/격자 반대로 채우기/grid-anti-fill.py
@@ -4,15 +4,26 @@ arr = [
     [0 for _ in range(n)]
     for _ in range(n)
 ]
-for i in range(n - 1, -1, -1):
-    if i % 2 != 0:
-        for j in range(n - 1, -1, -1):
-            arr[j][i] = num
-            num += 1
-    else:
-        for j in range(n):
-            arr[j][i] = num
-            num += 1
+if n % 2 == 0:
+    for i in range(n - 1, -1, -1):
+        if i % 2 != 0:
+            for j in range(n - 1, -1, -1):
+                arr[j][i] = num
+                num += 1
+        else:
+            for j in range(n):
+                arr[j][i] = num
+                num += 1
+else:
+    for i in range(n - 1, -1, -1):
+        if i % 2 == 0:
+            for j in range(n - 1, -1, -1):
+                arr[j][i] = num
+                num += 1
+        else:
+            for j in range(n):
+                arr[j][i] = num
+                num += 1
 for i in arr:
     for j in i:
         print(j, end = " ")

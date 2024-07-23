f82542a26bf67696f4c09e6236b498783a0e30ef
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-23 11:04:02 +0900

    Title: 두 숫자의 차의 최솟값, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -0,0 +1,7 @@
+n = int(input())
+arr = list(map(int, input().split()))
+answer = 2
+for i in range(n - 1):
+    if arr[i + 1] - arr[i] <= answer:
+        answer = arr[i + 1] - arr[i]
+print(answer)

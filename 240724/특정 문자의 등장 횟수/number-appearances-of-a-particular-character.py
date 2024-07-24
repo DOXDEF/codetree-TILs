4c203c8c4dbd89a3ee72858e49f587a1e70536b1
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-24 14:29:59 +0900

    Title: 특정 문자의 등장 횟수, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240724/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,2 +1,9 @@
 n = input()
-print(n.count('ee'), n.count('eb'))
+cnt_1 = 0
+cnt_2 = 0
+for i in range(len(n) - 1):
+    if n[i] + n[i + 1] == 'ee':
+        cnt_1 += 1
+    elif n[i] + n[i + 1] == 'eb':
+        cnt_2 += 1
+print(cnt_1, cnt_2)

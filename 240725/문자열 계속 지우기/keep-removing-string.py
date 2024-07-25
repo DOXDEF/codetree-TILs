3ff203f937a4b54eeeb556cbf069015b78839c66
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-25 12:54:24 +0900

    Title: 문자열 계속 지우기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240725/문자열 계속 지우기/keep-removing-string.py
+++ 240725/문자열 계속 지우기/keep-removing-string.py
@@ -1,13 +1,16 @@
 x = input()
 y = input()
+cnt = 0
 while True:
     for i in range(len(x) - (len(y) - 1)):
         x_slice = x[i:len(y) + i]
         if x_slice == y:
             x = x[:i] + x[len(y) + i:]
+            cnt += 1
             break
         else:
             pass
     if len(x) <= len(y):
         break
-print(x)
+if cnt != 0:
+    print(x)

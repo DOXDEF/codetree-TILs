61c33460f5cf9efe573b6eb221167512a7cf1eb3
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-26 20:31:38 +0900

    Title: 함수를 이용한 소수 판별, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240726/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -5,10 +5,11 @@ def is_dec(num):
     return True
 
 a, b = map(int, input().split())
-if b == 1:
-    print(1)
 sum_dec = 0
-for i in range(a, b + 1):
-    if is_dec(i):
-        sum_dec += i
-print(sum_dec)
+if b == 1:
+    print(0)
+else:
+    for i in range(a, b + 1):
+        if is_dec(i):
+            sum_dec += i
+    print(sum_dec)

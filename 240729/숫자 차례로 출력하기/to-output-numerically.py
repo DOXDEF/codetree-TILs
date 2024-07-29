6261e46f788ce3a552c8817307f2474b095cf5a2
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-29 13:58:13 +0900

    Title: 숫자 차례로 출력하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240729/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,18 @@
+def up(n):
+    if n == 0:
+        return
+    
+    up(n - 1)
+    print(n, end = " ")
+
+def down(n):
+    if n == 0:
+        return
+    
+    print(n, end = " ")
+    down(n - 1)
+
+n = int(input())
+up(n)
+print()
+down(n)

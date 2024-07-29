b60285d329389641af2f6e32c470c5d04919ac58
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-07-29 17:01:31 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,14 @@
+num_max = 0
+def maxi(n, arr):
+    global num_max
+    if n == 1:
+        return num_max + arr[0]
+    maxi(n - 1, arr)
+    if num_max != arr[0]:
+        if num_max < arr[n - 1]:
+            num_max = arr[n - 1]
+
+n = int(input())
+arr = list(map(int, input().split()))
+maxi(n, arr)
+print(num_max)

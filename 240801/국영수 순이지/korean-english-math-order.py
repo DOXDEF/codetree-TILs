c45fe62ff53ddf8100e3ed0364a775a736ab6c6c
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-01 15:10:53 +0900

    Title: 국영수 순이지, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240801/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,17 @@
+class Score:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+scores = []
+answer = []
+n = int(input())
+for _ in range(n):
+    a, b, c, d = input().split()
+    scores.append(Score(a, int(b), int(c), int(d)))
+sorted_scores = sorted(scores, key=lambda x: (x.kor, x.eng, x.math), reverse = True)
+
+for i in range(n):
+    print(sorted_scores[i].name, sorted_scores[i].kor, sorted_scores[i].eng, sorted_scores[i].math)

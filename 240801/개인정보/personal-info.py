a139b83a3bbd510bf00fa892eecd7edbc21f3929
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-01 17:00:16 +0900

    Title: 개인정보, Time: 212ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/개인정보/personal-info.py
+++ 240801/개인정보/personal-info.py
@@ -8,4 +8,14 @@ students = []
 for i in range(5):
     a, b, c = map(str, input().split())
     students.append(Student(a, b, c))
-sorted_students = sorted(students, key = lambda x: x.)
+
+sorted_students1 = sorted(students, key = lambda x: x.name)
+print("name")
+for i in range(5):
+    print(sorted_students1[i].name, sorted_students1[i].height, sorted_students1[i].weight)
+    
+sorted_students2 = sorted(students, key = lambda x: x.height, reverse = True)
+print("""
+height""")
+for i in range(5):
+    print(sorted_students2[i].name, sorted_students2[i].height, sorted_students2[i].weight)

c91ff900094254801c43b5b377aefe8daae276ed
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-01 13:27:55 +0900

    Title: 사는 지역, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/사는 지역/where-live.py
+++ 240801/사는 지역/where-live.py
@@ -3,16 +3,15 @@ class Human:
         self.name = name
         self.number = number
         self.country = country
-    def __repr__(self):
-        return repr((self.name, self.number, self.country))
 
 humans = []
+answer = []
 n = int(input())
 for _ in range(n):
     a, b, c = input().split()
     humans.append(Human(a, b, c))
-sorted(humans, key=lambda x: x.name)
-answer = humans[n - 1]
+sorted_humans = sorted(humans, key=lambda x: x.name)
+answer = sorted_humans[n - 1]
 print("name " + answer.name)
 print("addr " + answer.number)
 print("city " + answer.country)

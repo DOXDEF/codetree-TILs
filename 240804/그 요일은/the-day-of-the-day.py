fedc277f29f7643752d95747521daa38ee6bdda7
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-04 17:12:43 +0900

    Title: 그 요일은, Time: 131ms, Memory: 73MB, Status: Wrong Answer - CodeTree

--- 240804/그 요일은/the-day-of-the-day.py
+++ 240804/그 요일은/the-day-of-the-day.py
@@ -2,18 +2,11 @@ m1, d1, m2, d2 = map(int, input().split())
 day = input()
 arr1 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 arr2 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-sum1, sum2 = 0, 0
-for i in range(m1 - 1):
-    sum1 += arr1[i]
-sum1 += d1
-for i in range(m2 - 1):
-    sum2 += arr1[i]
-sum2 += d2
-cnt_day = 0
+sum1 = sum(arr1[:m1 - 1]) + d1
+sum2 = sum(arr1[:m2 - 1]) + d2
 
-cnt_day = (sum2 - sum1) // 7
-if (sum2 - sum1) % 7 == arr2.index(day):
-    cnt_day += 1
-if day == 'Mon':
-    cnt_day += 1
+cnt_day = 0
+for i in range(sum2 - sum1):
+    if arr2[i % 7] == day:
+        cnt_day += 1
 print(cnt_day)

875ea106fe6dfbd3598aafc1413b815af601eb2b
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-06 00:47:55 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240806/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -4,11 +4,11 @@ n = int(input())
 for _ in range(n):
     a, b = map(str, input().split())
     if b == 'R':
-        for i in range(point, point + int(a)):
-            arr[i] += 1
-        point += int(a)
+        for i in range(int(a)):
+            arr[point] += 1
+            point += 1
     else:
-        for i in range(point, point - int(a), -1):
-            arr[i] += 1
-        point -= int(a)
+        for i in range(int(a)):
+            point -= 1
+            arr[point] += 1
 print(len(arr) - (arr.count(0) + arr.count(1)))

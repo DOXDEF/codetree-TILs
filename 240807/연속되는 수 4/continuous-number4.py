a3a169ef9f22eab3308580e8fd17e3cdf06adea8
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-07 23:26:00 +0900

    Title: 연속되는 수 4, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = []
+part = []
+cnt = 0
+for _ in range(n):
+    a = int(input())
+    arr.append(a)
+for i in range(1, len(arr)):
+    if arr[i - 1] < arr[i]:
+        cnt += 1
+    else:
+        part.append(cnt)
+        cnt = 1
+part.append(cnt)
+part.sort(reverse = True)
+print(part[0])

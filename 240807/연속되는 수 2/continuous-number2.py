8ff0ff7bcdefa5cb93e8ef9709d1ca678f925a57
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-07 17:54:23 +0900

    Title: 연속되는 수 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,14 @@
+n = int(input())
+combo = 0
+cnt = 1
+max_cnt = 0
+for _ in range(n):
+    a = int(input())
+    if combo != a:
+        combo = a
+        if max_cnt < cnt:
+            max_cnt = cnt
+        cnt = 1
+    else:
+        cnt += 1
+print(max_cnt)

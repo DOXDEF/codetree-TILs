3bc6935ffd97fdd2246c3e477e8e4c9d642c54c8
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-07 17:45:21 +0900

    Title: 계속 중첩되는 사각형, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240807/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 240807/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -16,6 +16,9 @@ for k in range(n):
         x1, y1, x2, y2 = map(int, input().split())
         for i in range(x1 + 100, x2 + 100):
             for j in range(y1 + 100, y2 + 100):
-                arr[i][j] = 2
-                cnt += 1
+                if arr[i][j] == 2:
+                    pass
+                else:
+                    arr[i][j] = 2
+                    cnt += 1
 print(cnt)

8275a4ea00a2e62cc17677b6324ae395f824c08b
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-07 23:04:34 +0900

    Title: 연속되는 수 3, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240807/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr = []
+part = []
+sign = 0
+cnt = 0
+for _ in range(n):
+    a = int(input())
+    arr.append(a)
+for i in range(len(arr)):
+    if arr[i] > 0:
+        if sign == 1:
+            cnt += 1
+        else:
+            part.append(cnt)
+            sign = 1
+            cnt = 1
+    else:
+        if sign == -1:
+            cnt += 1
+        else:
+            part.append(cnt)
+            sign = -1
+            cnt = 1
+part.sort(reverse = True)
+print(part[0])

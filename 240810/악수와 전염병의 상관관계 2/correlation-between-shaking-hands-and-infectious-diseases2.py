c22d723461f574bb818a52268ea1ad200abe1646
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-10 17:33:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240810/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,18 @@
+n, k, p, t = map(int, input().split())
+timeline = []
+arr_virus = [k for _ in range(n)]
+arr_infect = [0 for _ in range(n)]
+arr_infect[p - 1] = 1
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    timeline.append([a, b, c])
+timeline.sort()
+for i in range(len(timeline)):
+    if (arr_infect[timeline[i][1] - 1] == 1 and arr_infect[timeline[i][2] - 1] == 0 and arr_virus[timeline[i][1] - 1] != 0):
+        arr_infect[timeline[i][2] - 1] = 1
+        arr_virus[timeline[i][1] - 1] -= 1
+    elif (arr_infect[timeline[i][2] - 1] == 1 and arr_infect[timeline[i][1] - 1] == 0 and arr_virus[timeline[i][2] - 1] != 0):
+        arr_infect[timeline[i][1] - 1] = 1
+        arr_virus[timeline[i][2] - 1] -= 1
+for i in arr_infect:
+    print(i, end = "")

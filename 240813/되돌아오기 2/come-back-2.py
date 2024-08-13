d32914311f35272ffcb3c66b5872d6c6862d227c
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-13 18:46:36 +0900

    Title: 되돌아오기 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240813/되돌아오기 2/come-back-2.py
@@ -0,0 +1,20 @@
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+x, y = 0, 0
+n = input()
+idx = 100000
+cnt = 0
+answer = -1
+for i in range(len(n)):
+    if n[i] == 'L':
+        idx -= 1
+        cnt += 1
+    elif n[i] == 'R':
+        idx += 1
+        cnt += 1
+    else:
+        x, y = x + dx[idx % 4], y + dy[idx % 4]
+        cnt += 1
+        if x == 0 and y == 0:
+            answer = cnt
+            break
+print(answer)

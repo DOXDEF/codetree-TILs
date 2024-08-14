008806cbf8d1f9c7db42ba9e53d4521f41bfc4cc
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-14 21:16:18 +0900

    Title: 격자 위의 편안한 상태, Time: 174ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240814/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+arr_2d = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    arr_2d[x - 1][y - 1] = 1
+    cnt = 0
+    cnt_one = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr_2d[nx][ny] == 1:
+            cnt = 1
+    if cnt == 1:
+        cnt_one = 1
+    print(cnt_one)

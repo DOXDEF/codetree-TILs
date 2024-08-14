1d92be05cd55e2583c15ddd9126b4cbed2c38c77
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-14 22:07:03 +0900

    Title: 모이자, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240814/모이자/gather.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+min_dist = 9999
+for i in range(n):
+    dist = 0
+    for j in range(n):
+        dist += arr[j] * abs(i - j)
+    if dist < min_dist:
+        min_dist = dist
+print(min_dist)

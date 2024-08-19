edb77cbaea06bd89593cb0074211fab500e91e67
DOXDEF <77396001+DOXDEF@users.noreply.github.com>
2024-08-19 23:42:16 +0900

    Title: 이상한 진수 2, Time: 101ms, Memory: 25MB, Status: Passed - CodeTree

--- 240819/이상한 진수 2/awkward-digits-2.py
+++ 240819/이상한 진수 2/awkward-digits-2.py
@@ -1,5 +1,5 @@
 def find_max_N(a):
-    max_value = int(a, 2)  # 현재 주어진 이진수를 정수로 변환
+    possible_values = []
     
     # 모든 자리를 하나씩 바꿔가며 가능한 모든 숫자 생성
     for i in range(len(a)):
@@ -10,14 +10,11 @@ def find_max_N(a):
             # 1을 0으로 바꿔본다.
             modified_a = a[:i] + '0' + a[i+1:]
         
-        # 수정된 이진수를 정수로 변환
-        new_value = int(modified_a, 2)
-        
-        # 최댓값 갱신
-        if new_value > max_value:
-            max_value = new_value
+        # 수정된 이진수를 정수로 변환하여 리스트에 추가
+        possible_values.append(int(modified_a, 2))
     
-    return max_value
+    # 가능한 값들 중 최댓값을 반환
+    return max(possible_values)
 
 # 테스트
 a = input()
